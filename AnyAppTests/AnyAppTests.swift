d4a0e083c7f1a5f90db9737f2e4b16a633cb9cda
Hanh Vo <hanhvo@Hanhs-MBP.attlocal.net>
2023-02-08 20:01:17 -0600

    Unit test CI set up

---
+++ AnyAppTests/AnyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnyAppTests.swift
+//  AnyAppTests
+//
+//  Created by tdolenko on 06.07.22.
+//
+
+import XCTest
+@testable import AnyApp
+
+class AnyAppTests: XCTestCase {
+
+    func testExample() throws{
+        XCTAssertEqual(2 + 2, 4)
+    }
+
+}
